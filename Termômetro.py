5891577b07edc3cddcf60696fa56561223c8de46
LordGabee <109282881+HauntedLordGabe@users.noreply.github.com>
2022-08-01 18:43:18 -0300

    Funções: Termômetro

---
+++ Termômetro.py
@@ -0,0 +1,9 @@
+def fahrenheit(T):
+    formulaf = (T * 9/5 + 32)
+    print(f'Fahrenheit: {formulaf:.2f}')
+def kelvin(T):
+    formulak = T + 273.15
+    print(f'Kelvin: {formulak}')
+
+#(32 °F − 32) × 5/9 = 0 °C fah
+#0 K − 273,15 = -273,1 °C kel
