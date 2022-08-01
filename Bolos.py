ee260e2a01d380de570c8884bbae234a5c95a37a
LordGabee <109282881+HauntedLordGabe@users.noreply.github.com>
2022-08-01 18:31:00 -0300

    Variáveis, expressões e instruções: Bolos

---
+++ Bolos.py
@@ -0,0 +1,10 @@
+#
+
+x, y = input().split()
+
+z = 3.25
+
+valor_de_y = int(y)
+valor_da_compra = valor_de_y * z
+
+print(f'Foram {y} pedaços de bolo de {x}, então fica {valor_da_compra:.2f} reais')
