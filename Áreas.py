7abfebf70d4bbdc360b47002e74a7869d0cc21af
LordGabee <109282881+HauntedLordGabe@users.noreply.github.com>
2022-08-01 18:46:51 -0300

    Funções: Áreas

---
+++ Áreas.py
@@ -0,0 +1,9 @@
+def areas(a,b,c):
+    retangulo = a * b
+    triangulo = b * c // 2
+    trap1 = b + c 
+    trap2 = trap1 * a
+    trap3 = trap2 // 2
+    print(retangulo)
+    print(triangulo)
+    print(trap3)
