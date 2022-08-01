93cf6e35e2389d014690705534095c7d59abfd0c
LordGabee <109282881+HauntedLordGabe@users.noreply.github.com>
2022-08-01 18:41:00 -0300

    Variáveis, expressões e instruções: Troca de Esmeraldas

---
+++ Troca de Esmeraldas.py
@@ -0,0 +1,14 @@
+f = int(input())
+o = int(input())
+p = int(input())
+d = int(input())
+
+ferro = f
+pó = p * 2
+
+ferro_pó = ferro + pó
+ouro = o * 2
+diamante = d * 4
+fim = ferro_pó * ouro * diamante
+
+print(fim)
