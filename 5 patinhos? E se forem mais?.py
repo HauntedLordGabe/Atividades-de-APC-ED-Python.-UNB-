77899b03229f6724a72945747f66a02bc14d37b9
LordGabee <109282881+HauntedLordGabe@users.noreply.github.com>
2022-08-01 18:47:32 -0300

    Funções: 5 patinhos? E se forem mais?

---
+++ 5 patinhos? E se forem mais?.py
@@ -0,0 +1,6 @@
+def estrofe(n):
+    print(f'{n} patinhos\nForam passear\nAlém das montanhas\nPara brincar')
+    refrao(n-1)
+    print('Agora só falta aprender a fazer loop!')
+def refrao(n):
+    print(f'A mamãe gritou\nQuá, quá, quá, quá!\nMas só {n} patinhos\nVoltaram de lá')
